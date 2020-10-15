87eca28a6b0d727518121f69e398105802752865
Daniel C <56047938+dconford@users.noreply.github.com>
2020-10-15 10:15:38 -0500

    added basic-count component shell

---
+++ src/main/webapp/app/home/basic-count/basic-count.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-basic-count',
+  templateUrl: './basic-count.component.html',
+  styleUrls: ['./basic-count.component.scss']
+})
+export class BasicCountComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,9 +4,10 @@ import { RouterModule } from '@angular/router';
 import { SlmpdDataV4HSharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
+import { BasicCountComponent } from './basic-count/basic-count.component';
 
 @NgModule({
   imports: [SlmpdDataV4HSharedModule, RouterModule.forChild([HOME_ROUTE])],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent, BasicCountComponent]
 })
 export class SlmpdDataV4HHomeModule {}
